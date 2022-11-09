62ec11d2e140ba046c6ceafdf0b3793653768a0c
0007aman <116329076+0007aman@users.noreply.github.com>
2022-11-09 16:49:43 +0530

    Add files via upload

---
+++ email_pswd2.sh
@@ -0,0 +1,26 @@
+ans='y'
+echo "JAI SHREE RAM"
+echo " "
+echo "PLEASE ENTER YOUR DETAILS CORRECTLY TO GET YOUR USER ID AND PASSWORD"
+while test $ans=='y'
+do
+	echo " "
+	echo "Enter your full name (Surname Name Middle name : "
+	read nm
+	echo "Enter your Roll no : "
+	read rn
+	echo "Enter your Phone no : "
+	read num
+	p_nm=`echo $nm | cut -d " " -f2 | tr "[a-z]" "[A-Z]"`
+	s_nm=`echo $nm | cut -d " " -f1 | tr "[a-z]" "[A-Z]" `
+	p_num=`echo $num | cut -c1,2,3,4,5 `
+	usrnm=$p_nm"."$s_nm"."$p_num"@VES.AC.IN"
+	pswd=$p_nm"."$rn"@"$p_num
+	echo " "
+	echo "Your userid is : "$usrnm
+	echo "Your password is : "$pswd
+	echo " "
+	echo "Do you want to create more user id and passwors ?(y/n) "
+	read ans
+done
+
